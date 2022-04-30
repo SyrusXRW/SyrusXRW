2c6b9dba2ac6a596506b5cffe691adb6117c298e
SyrusXRW <Ittybitty100s@gmail.com>
2022-04-30 14:43:15 -0700

    Create move.cs

---
+++ move.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    public float rotationSpeed = 10.0f;
+    Rigidbody rb;
+
+    private Vector2 lastMousePosition;
+    private Vector3 movementDir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+        lastMousePosition = Input.mousePosition;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 newPosition = new Vector3(transform.position.x + (speed * Time.deltaTime),
+            transform.position.y, transform.position.z);
+        Vector3 moveRight = transform.right * movementDir.x;
+        Vector3 moveForward = transform.forward * movementDir.z;
+
+        Vector3 newPosition = transform.position + ((moveRight + moveForward) * speed * Time.deltaTime);
+
+        //rb.AddForce(movementDir * 50);
+        rb.MovePosition(newPosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 rotationAmount = mousePosition - lastMousePosition;
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Vector3 force = new Vector3(0.0f, jumpForce, 0.0f);
+            rb.AddForce(force, ForceMode.Impulse);
+        }
+
+        Vector3 capsuleRotate = new Vector3(0.0f, rotationAmount.x * rotationSpeed * Time.deltaTime, 0.0f);
+        transform.Rotate(capsuleRotate);
+
+        movementDir = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            movementDir.z = 1.0f;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            movementDir.z = -1.0f;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            movementDir.x = -1.0f;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            movementDir.x = 1.0f;
+        }
+
+        lastMousePosition = mousePosition;
+    }
+}
